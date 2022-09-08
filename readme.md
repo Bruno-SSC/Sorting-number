a75a365a9a80a91fb81375b5b1aadbe51631fea4
Bruno <faithkidbruno@gmail.com>
2022-09-08 18:30:13 -0300

    first and last

---
+++ readme.md
@@ -0,0 +1,24 @@
+
+<h1 align="center"> Sorteio 💻 </h1>
+
+## Briefing 📄
+
+Aplicação web com finalidade de sortear um número de forma aleatória.
+
+<h2 align="left"> Objetivos 📌 </h2>
+
+- Melhorar skills como front-end developer
+
+---
+<h3 align="center"> Design 📝 </h3>
+
+<div height="590em"><img src="./images/desktop-design.jpg" alt="imagem"></div>
+
+<h3 align="center"> Tecnologias utilizadas 🤖 </h3>
+
+> <div align="center"><img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white"><img src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white"><img src="https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E"></div>
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,41 @@
+const sortBTN = document.querySelector('#sortBTN')
+const resultado = document.querySelector('#resultado h1')
+var interval
+
+
+sortBTN.addEventListener('click', () => {
+
+    interval = setInterval(changeNum, 1000)
+    setTimeout(stop, 6000)
+
+})
+
+function changeNum() {
+
+    let newNum = Math.round(Math.random() * 100)
+    resultado.innerHTML = newNum
+
+    let animateText = {
+        display: ['inline'],
+        fontSize: [0, '7rem'],
+        transform: ['rotate(360deg)']
+    }
+
+    let animate = {
+        display: ['grid'],
+        width: [0, '15vw'],
+        height: [0, '30vh'],
+    }
+
+    let animateOption = {
+        duration: 300,
+        iterations: 1
+    }
+
+    resultado.animate(animateText, animateOption)
+    resultado.parentElement.animate(animate, animateOption)
+}
+
+function stop() {
+    clearInterval(interval)
+}
