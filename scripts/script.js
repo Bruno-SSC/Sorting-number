9254a209104c7ec22e075cdfabce25087b940da6
Bruno <faithkidbruno@gmail.com>
2022-10-30 18:38:36 -0300

    changed result effect and responsive fixes

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,10 @@ const sortBTN = document.querySelector('#sortBTN')
 const resultado = document.querySelector('#result h1')
 var interval
 
-
 sortBTN.addEventListener('click', () => {
 
     interval = setInterval(changeNum, 1000)
-    setTimeout(stop, 6000)
+    setTimeout(stop, 4000)
 
 })
 
@@ -15,27 +14,12 @@ function changeNum() {
     let newNum = Math.round(Math.random() * 100)
     resultado.innerHTML = newNum
 
-    let animateText = {
-        display: ['inline'],
-        fontSize: [0, '7rem'],
-        transform: ['rotate(360deg)']
-    }
-
-    let animate = {
-        display: ['grid'],
-        width: [0, '15vw'],
-        height: [0, '30vh'],
-    }
-
-    let animateOption = {
-        duration: 300,
-        iterations: 1
-    }
-
-    resultado.animate(animateText, animateOption)
-    resultado.parentElement.animate(animate, animateOption)
 }
 
 function stop() {
     clearInterval(interval)
+    resultado.classList.add('done')
+    setTimeout(() => {
+        resultado.classList.remove('done')
+    }, 3000);
 }
